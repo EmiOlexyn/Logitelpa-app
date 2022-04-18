69b0e5d283d7a389a595cec46e06d6828d52ebd0
Emiliano Olexyn <69089139+EmiOlexyn@users.noreply.github.com>
2022-04-18 19:28:11 -0300

    Navbar add
    Commit con navbar agregado

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './NavBar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
       <h1>Hola mundo con React!</h1>
     </div>
   );
---
+++ src/NavBar.js
@@ -0,0 +1,16 @@
+const Navbar = () => {
+    
+
+
+return (
+    <div>
+        <li>Inicio</li>
+        <li>Productos en ofertas</li>
+        <li>¿Quienes somos?</li>
+    </div>
+)
+
+
+}
+
+export default Navbar
